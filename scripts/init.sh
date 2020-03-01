1d7d45077c262f1f1fd6877be1034c775e8ab2eb
jtheisen <pass-bb@zokfotpik.de>
2020-03-01 21:18:39 +0100

    Let's try a script

---
+++ scripts/init.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
+add-apt-repository universe -y
+apt-get update -y
+apt-get install apt-transport-https -y
+apt-get update -y
+apt-get install dotnet-sdk-3.1 -y
+cd /var/www-src
+dotnet publish --configuration Release -o /var/www
+cp scripts/kestrel.service /etc/systemd/system/kestrel.service
+systemctl --now enable kestrel
